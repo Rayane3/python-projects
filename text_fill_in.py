64ff0f12243a9b58e73846876d26c246859887f3
rayane3main <bouayadr39@gmail.com>
2020-12-10 23:27:34 +0100

    initial-commit

---
+++ text_fill_in.py
@@ -0,0 +1,11 @@
+print("describe a person by giving us their name and four adjectives!")
+
+name = input("name: ")
+adj = input("1st adjective: ")
+adj1 = input("2nd adjective: ")
+adj2 = input("3rd adjective: ")
+adj3 = input("4th adjective: ")
+
+madlib = (name + " is such a " + adj +" person." " He is so " + adj1 + " and " + adj2 + ". He is also " + adj3 )
+
+print(madlib)
